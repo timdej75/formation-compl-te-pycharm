5671e6cf4e113d918d7b875f486fb31ee971de63
Dejan <dej.tim@gmail.com>
2022-04-24 02:06:05 +0200

    Commit d'initialisation 4

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Nouvelle ligne de commentaire 24/04/2022
 
 
 def print_hi(name):
